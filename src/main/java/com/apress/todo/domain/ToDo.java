a060830f015f282d4ab411d7b9f2f62368b7b9ff
Eman <eman.cse2008@gmail.com>
2020-08-25 12:00:19 +0200

    initial commit

---
+++ src/main/java/com/apress/todo/domain/ToDo.java
@@ -0,0 +1,12 @@
+package com.apress.todo.domain;
+
+import java.time.LocalDateTime;
+
+public class ToDo {
+    private String id;
+    private String description;
+    private boolean completed;
+    private LocalDateTime created;
+    private LocalDateTime modified;
+}
+
